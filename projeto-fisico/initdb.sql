d5beae8f800fac85e2b5dccd48d3a329d3a7766c
MrHenri <henrique.oliveira.gomes@gmail.com>
2022-09-24 15:22:54 -0300

    adjusts

--- projeto-fisico/initdb.sql
+++ projeto-fisico/initdb.sql
@@ -1,136 +1,131 @@
 CREATE TABLE Herois(
-    nome VARCHAR(80) NOT NULL,
-    id_heroi INT AUTO_INCREMENT PRIMARY KEY,
+    heroi VARCHAR(80) PRIMARY KEY,
     estado VARCHAR(12) DEFAULT 'Vivo',
-    mentor INT, 
-    FOREIGN KEY (mentor) REFERENCES Herois(id_heroi)
+    mentor VARCHAR(80), 
+    FOREIGN KEY (mentor) REFERENCES Herois(heroi)
 );
 
 CREATE TABLE Feitos (
-    id_heroi INT NOT NULL,
-    feitos_realizados VARCHAR(280),
-    CONSTRAINT pk_feitos PRIMARY KEY (id_heroi, feitos_realizados),
-    CONSTRAINT fk_feitos FOREIGN KEY (id_heroi) REFERENCES Herois (id_heroi)
+    heroi VARCHAR(80) NOT NULL,
+    feito_realizado VARCHAR(280),
+    CONSTRAINT pk_feitos PRIMARY KEY (heroi, feito_realizado),
+    CONSTRAINT fk_feitos FOREIGN KEY (heroi) REFERENCES Herois (heroi)
 );
 
 CREATE TABLE Comuns (
-	id_heroi int PRIMARY KEY,
+	heroi VARCHAR(80) PRIMARY KEY,
     especialidade VARCHAR(80),
-    FOREIGN KEY (id_heroi) REFERENCES Herois(id_heroi) 
+    FOREIGN KEY (heroi) REFERENCES Herois(heroi)
 );
 
 CREATE TABLE Poderes (
-	grau ENUM('1', '2', '3', '4', '5'),
-    titulo VARCHAR(80) PRIMARY KEY,
+	grau INT NOT NULL CHECK (grau BETWEEN 0 AND 5),
+    poder VARCHAR(80) PRIMARY KEY,
     descricao VARCHAR(280), 
-    id_heroi int NOT NULL,
-    FOREIGN KEY (id_heroi) REFERENCES Herois(id_heroi)
+    heroi VARCHAR(80) NOT NULL,
+    FOREIGN KEY (heroi) REFERENCES Herois(heroi)
 );
 
 
 CREATE TABLE Artefatos(
-    nome VARCHAR(80) PRIMARY KEY,
-    id_heroi INT,
-    FOREIGN KEY (id_heroi) REFERENCES Herois(id_heroi)
-);
-
-CREATE TABLE Equipes (
-    nome VARCHAR(80) NOT NULL,
-    codigo INT PRIMARY KEY AUTO_INCREMENT
-);
-
-CREATE TABLE HeroiEquipe (
-    id_heroi INT NOT NULL,
-    cod_equipe INT NOT NULL,
-    FOREIGN KEY (id_heroi) REFERENCES Herois(id_heroi),
-    FOREIGN KEY (cod_equipe) REFERENCES Equipes(codigo)
-);
-
-CREATE TABLE Bases_De_Operacao (
-    numero int AUTO_INCREMENT,
-    cod_equipe int,
-    PRIMARY KEY (numero, cod_equipe),
-    FOREIGN KEY (cod_equipe) REFERENCES Equipes(codigo)
-);
-
-CREATE TABLE Viloes (
-	codinome VARCHAR(20) PRIMARY KEY
+    artefato VARCHAR(80) PRIMARY KEY,
+    heroi VARCHAR(80),
+    FOREIGN KEY (heroi) REFERENCES Herois(heroi)
 );
 
 Create TABLE Localizacoes(
-	latitude DECIMAL(8,6),
-    longitude DECIMAL(9,6),
-    cod_equipe int UNIQUE,
-    PRIMARY KEY (latitude, longitude),
-    FOREIGN KEY (cod_equipe) REFERENCES Equipes(codigo)
+    regiao VARCHAR(80) PRIMARY KEY,
+	latitude NUMBER (*,15) NOT NULL CHECK (latitude BETWEEN -90 AND 90),
+    longitude NUMBER (*,15) NOT NULL CHECK (longitude BETWEEN -180 AND 180)
+);
+
+CREATE TABLE Equipes (
+    equipe VARCHAR(80) PRIMARY KEY,
+    regiao VARCHAR(80),
+    FOREIGN KEY (regiao) REFERENCES Localizacoes(regiao)
+);
+
+CREATE TABLE HeroiEquipe (
+    heroi VARCHAR(80) NOT NULL,
+    equipe VARCHAR(80) NOT NULL,
+    FOREIGN KEY (heroi) REFERENCES Herois(heroi),
+    FOREIGN KEY (equipe) REFERENCES Equipes(equipe)
+);
+
+-- CREATE TABLE Bases_De_Operacao (
+--     numero int AUTO_INCREMENT,
+--     equipe int,
+--     PRIMARY KEY (numero, equipe),
+--     FOREIGN KEY (equipe) REFERENCES Equipes(codigo)
+-- );
+
+CREATE TABLE Viloes (
+	vilao VARCHAR(80) PRIMARY KEY
 );
 
 CREATE TABLE Combates(
-	latitude DECIMAL(8,6),
-    longitude Decimal(9,6),
-    id_heroi int,
-    cod_vilao varchar(20),
-    dia date,
-    PRIMARY KEY (id_heroi, cod_vilao, dia, latitude, longitude),
-    FOREIGN KEY (id_heroi) REFERENCES Herois(id_heroi),
-    FOREIGN KEY (cod_vilao) REFERENCES Viloes(codinome),
-    FOREIGN KEY (latitude, longitude) REFERENCES Localizacoes(latitude, longitude)
+	localizacao VARCHAR(80),
+    heroi VARCHAR(80),
+    vilao VARCHAR(80),
+    dia DATE,
+    PRIMARY KEY (heroi, vilao, dia, localizacao),
+    FOREIGN KEY (heroi) REFERENCES Herois(heroi),
+    FOREIGN KEY (vilao) REFERENCES Viloes(vilao),
+    FOREIGN KEY (localizacao) REFERENCES Localizacoes(regiao)
 );
 
 CREATE TABLE Vitimas(
-    latitude DECIMAL(8,6) NOT NULL,
-    longitude Decimal(9,6) NOT NULL,
-    id_heroi int NOT NULL,
-    cod_vilao varchar(20) NOT NULL,
-    dia date NOT NULL,
-    cpf varchar(12) PRIMARY KEY,
-    FOREIGN KEY (id_heroi, cod_vilao, dia, latitude, longitude) 
-    REFERENCES Combates(id_heroi, cod_vilao, dia, latitude, longitude)
+    localizacao VARCHAR(80) NOT NULL,
+    heroi INT NOT NULL,
+    vilao VARCHAR(20) NOT NULL,
+    dia DATE NOT NULL,
+    cpf VARCHAR(12) PRIMARY KEY,
+    FOREIGN KEY (heroi, vilao, dia, localizacao),
+    REFERENCES Combates(heroi, vilao, dia, localizacao)
 );
 
-INSERT INTO Herois(nome) VALUES("Iron Man");
-INSERT INTO Herois(nome) VALUES("Captain America");
-INSERT INTO Herois(nome) VALUES("Hulk");
-INSERT INTO Herois(nome) VALUES("hawkeye");
-INSERT INTO Herois(nome) VALUES("Black Widow");
-INSERT INTO Herois(nome) VALUES("Thor");
-INSERT INTO Herois(nome) VALUES("Star Lord");
-INSERT INTO Herois(nome) VALUES("Gamora");
-INSERT INTO Herois(nome) VALUES("Groot");
-INSERT INTO Herois(nome) VALUES("Drax");
-INSERT INTO Herois(nome) VALUES("Rocket Racoon");
-INSERT INTO Herois(nome, mentor) VALUES("Spider Man", 1);
-INSERT INTO Herois(nome, mentor) VALUES("Winter Soldier", 2);
-INSERT INTO Herois(nome, mentor) VALUES("Falcon", 2);
+INSERT INTO Herois(heroi) VALUES('Iron Man');
+INSERT INTO Herois(heroi) VALUES('Captain America');
+INSERT INTO Herois(heroi) VALUES('Hulk');
+INSERT INTO Herois(heroi) VALUES('hawkeye');
+INSERT INTO Herois(heroi) VALUES('Black Widow');
+INSERT INTO Herois(heroi) VALUES('Thor');
+INSERT INTO Herois(heroi) VALUES('Star Lord');
+INSERT INTO Herois(heroi) VALUES('Gamora');
+INSERT INTO Herois(heroi) VALUES('Groot');
+INSERT INTO Herois(heroi) VALUES('Drax');
+INSERT INTO Herois(heroi) VALUES('Rocket Racoon');
+INSERT INTO Herois(heroi, mentor) VALUES('Spider Man', 'Iron Man');
+INSERT INTO Herois(heroi, mentor) VALUES('Winter Soldier', 'Captain America');
+INSERT INTO Herois(heroi, mentor) VALUES('Falcon', 'Captain America');
+
+INSERT INTO Poderes(heroi, poder, grau) VALUES('Hulk', 'Super Força', 5);
+INSERT INTO Poderes(heroi, poder, grau) VALUES('Hulk', 'Regeneração', 5);
+
+INSERT INTO Feitos(heroi, feito_realizado) VALUES('Captain America', 'Veterano da Segunda Guerra Mundial');
+INSERT INTO Feitos(heroi, feito_realizado) VALUES('Iron Man', 'Lutou contra a invasão alienigena de Nova York em 2012');
+INSERT INTO Feitos(heroi, feito_realizado) VALUES('Captain America', 'Lutou contra a invasão alienigena de Nova York em 2012');
+INSERT INTO Feitos(heroi, feito_realizado) VALUES('Hulk', 'Lutou contra a invasão alienigena de Nova York em 2012');
+INSERT INTO Feitos(heroi, feito_realizado) VALUES('hawkeye', 'Lutou contra a invasão alienigena de Nova York em 2012');
+INSERT INTO Feitos(heroi, feito_realizado) VALUES('Black Widow', 'Lutou contra a invasão alienigena de Nova York em 2012');
+INSERT INTO Feitos(heroi, feito_realizado) VALUES('Thor', 'Lutou contra a invasão alienigena de Nova York em 2012');
 
 
-INSERT INTO Feitos(id_heroi, feitos_realizados) VALUES(2, "Veterano da Segunda Guerra Mundial");
-INSERT INTO Feitos(id_heroi, feitos_realizados) VALUES(1, "Lutou contra a invasão alienigena de Nova York em 2012");
-INSERT INTO Feitos(id_heroi, feitos_realizados) VALUES(2, "Lutou contra a invasão alienigena de Nova York em 2012");
-INSERT INTO Feitos(id_heroi, feitos_realizados) VALUES(3, "Lutou contra a invasão alienigena de Nova York em 2012");
-INSERT INTO Feitos(id_heroi, feitos_realizados) VALUES(4, "Lutou contra a invasão alienigena de Nova York em 2012");
-INSERT INTO Feitos(id_heroi, feitos_realizados) VALUES(5, "Lutou contra a invasão alienigena de Nova York em 2012");
-INSERT INTO Feitos(id_heroi, feitos_realizados) VALUES(6, "Lutou contra a invasão alienigena de Nova York em 2012");
+INSERT INTO Equipes(equipe) VALUES ('Avengers');
+INSERT INTO Equipes(equipe) VALUES ('Guardians Of The Galaxy');
 
 
-INSERT INTO Equipes(nome) VALUES ("Avengers");
-INSERT INTO Equipes(nome) VALUES ("Guardians Of The Galaxy");
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Iron Man','Avengers');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Captain America','Avengers');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Hulk','Avengers');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('hawkeye','Avengers');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Black Widow','Avengers');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Thor','Avengers');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Star Lord','Guardians Of The Galaxy');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Gamora','Guardians Of The Galaxy');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Groot','Guardians Of The Galaxy');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Drax','Guardians Of The Galaxy');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Rocket Racoon','Guardians Of The Galaxy');
+INSERT INTO HeroiEquipe(heroi, equipe) VALUES('Spider Man','Avengers');
 
-
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(1,1);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(2,1);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(3,1);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(4,1);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(5,1);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(6,1);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(7,2);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(8,2);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(9,2);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(10,2);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(11,2);
-INSERT INTO HeroiEquipe(id_heroi, cod_equipe) VALUES(12,1);
-
-
--- ALL Avengers
--- SELECT h.nome FROM Herois h, HeroiEquipe he, Equipes e
--- WHERE h.id_heroi = he.id_heroi AND he.cod_equipe = e.codigo AND e.nome = "Avengers";
+INSERT INTO Localizacoes(regiao, latitude, longitude) VALUES('Nova York', 40.894112553018076, -73.97263907468503);
