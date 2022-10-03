8332fb9c7879ddea3e5ed1b117ea6e3373eba29d
MrHenri <henrique.oliveira.gomes@gmail.com>
2022-10-03 18:57:59 -0300

    Constraints adjusts

--- projeto-fisico/projeto-fisico.sql
+++ projeto-fisico/projeto-fisico.sql
@@ -1,67 +1,75 @@
 CREATE TABLE Herois(
-    heroi VARCHAR(80) PRIMARY KEY,
+    heroi VARCHAR(80),
     estado VARCHAR(12) DEFAULT 'Vivo',
-    mentor VARCHAR(80), 
-    FOREIGN KEY (mentor) REFERENCES Herois(heroi)
+    mentor VARCHAR(80),
+    CONSTRAINT pk_heroi PRIMARY KEY (heroi),
+    CONSTRAINT fk_mentor FOREIGN KEY (mentor) REFERENCES Herois(heroi) ON DELETE SET NULL
 );
 
 CREATE TABLE Feitos (
-    heroi VARCHAR(80) NOT NULL,
     feito_realizado VARCHAR(280),
-    CONSTRAINT pk_feitos PRIMARY KEY (heroi, feito_realizado),
-    CONSTRAINT fk_feitos FOREIGN KEY (heroi) REFERENCES Herois (heroi)
+    heroi VARCHAR(80),
+    CONSTRAINT pk_feito PRIMARY KEY (heroi, feito_realizado),
+    CONSTRAINT fk_heroi_feito FOREIGN KEY (heroi) REFERENCES Herois(heroi) ON DELETE CASCADE
 );
 
 CREATE TABLE Comuns (
-	heroi VARCHAR(80) PRIMARY KEY,
-    especialidade VARCHAR(80),
-    FOREIGN KEY (heroi) REFERENCES Herois(heroi)
+	heroi VARCHAR(80),
+    especialidade VARCHAR(80) NOT NULL,
+    CONSTRAINT pk_comum PRIMARY KEY (heroi),
+    CONSTRAINT fk_heroi_comum FOREIGN KEY (heroi) REFERENCES Herois(heroi) ON DELETE CASCADE
 );
 
 CREATE TABLE Poderes (
 	grau INT NOT NULL CHECK (grau BETWEEN 0 AND 5),
-    poder VARCHAR(80) PRIMARY KEY,
-    descricao VARCHAR(280), 
+    poder VARCHAR(80),
+    descricao VARCHAR(280),
     heroi VARCHAR(80) NOT NULL,
-    FOREIGN KEY (heroi) REFERENCES Herois(heroi)
+    CONSTRAINT pk_poder PRIMARY KEY (poder),
+    CONSTRAINT fk_heroi_poder FOREIGN KEY (heroi) REFERENCES Herois(heroi) ON DELETE CASCADE
 );
 
 
 CREATE TABLE Artefatos(
-    artefato VARCHAR(80) PRIMARY KEY,
+    artefato VARCHAR(80),
     heroi VARCHAR(80),
-    FOREIGN KEY (heroi) REFERENCES Herois(heroi)
+    CONSTRAINT pk_artefato PRIMARY KEY (artefato),
+    CONSTRAINT fk_heroi_artefato FOREIGN KEY (heroi) REFERENCES Herois(heroi) ON DELETE SET NULL
 );
 
 Create TABLE Localizacoes(
-    regiao VARCHAR(80) PRIMARY KEY,
+    regiao VARCHAR(80),
 	latitude NUMBER (*,15) NOT NULL CHECK (latitude BETWEEN -90 AND 90),
-    longitude NUMBER (*,15) NOT NULL CHECK (longitude BETWEEN -180 AND 180)
+    longitude NUMBER (*,15) NOT NULL CHECK (longitude BETWEEN -180 AND 180),
+    CONSTRAINT pk_regiao PRIMARY KEY (regiao)
 );
 
 CREATE TABLE Equipes (
-    equipe VARCHAR(80) PRIMARY KEY,
+    equipe VARCHAR(80),
     regiao VARCHAR(80),
-    FOREIGN KEY (regiao) REFERENCES Localizacoes(regiao)
+    CONSTRAINT pk_equipe PRIMARY KEY (equipe),
+    CONSTRAINT fk_equipe_regiao FOREIGN KEY (regiao) REFERENCES Localizacoes(regiao) ON DELETE SET NULL
 );
 
 CREATE TABLE HeroiEquipe (
-    heroi VARCHAR(80) NOT NULL,
-    equipe VARCHAR(80) NOT NULL,
-    FOREIGN KEY (heroi) REFERENCES Herois(heroi),
-    FOREIGN KEY (equipe) REFERENCES Equipes(equipe)
+    heroi VARCHAR(80),
+    equipe VARCHAR(80),
+    CONSTRAINT pk_heroi_equipe PRIMARY KEY (heroi, equipe),
+    CONSTRAINT fk_heroi_heroiequipe FOREIGN KEY (heroi) REFERENCES Herois(heroi) ON DELETE CASCADE,
+    CONSTRAINT fk_equipe_heroiequipe FOREIGN KEY (equipe) REFERENCES Equipes(equipe) ON DELETE CASCADE
 );
 
 CREATE TABLE Bases_De_Operacoes (
    base VARCHAR(80),
    chefe VARCHAR(80),
    equipe VARCHAR(80),
-   PRIMARY KEY (base, equipe),
-   FOREIGN KEY (equipe) REFERENCES Equipes(equipe)
+   CONSTRAINT pk_base PRIMARY KEY (base, equipe),
+   CONSTRAINT fk_equipe_base FOREIGN KEY (equipe) REFERENCES Equipes(equipe)
 );
 
 CREATE TABLE Viloes (
-	vilao VARCHAR(80) PRIMARY KEY
+	vilao VARCHAR(80),
+    CONSTRAINT pk_vilao PRIMARY KEY (vilao)
 );
 
 CREATE TABLE Combates(
@@ -69,22 +77,24 @@ CREATE TABLE Combates(
     heroi VARCHAR(80),
     vilao VARCHAR(80),
     dia DATE,
-    PRIMARY KEY (heroi, vilao, dia, localizacao),
-    FOREIGN KEY (heroi) REFERENCES Herois(heroi),
-    FOREIGN KEY (vilao) REFERENCES Viloes(vilao),
-    FOREIGN KEY (localizacao) REFERENCES Localizacoes(regiao)
+    CONSTRAINT pk_combate PRIMARY KEY (heroi, vilao, dia, localizacao),
+    CONSTRAINT fk_heroi_combate FOREIGN KEY (heroi) REFERENCES Herois(heroi) ON DELETE CASCADE,
+    CONSTRAINT fk_vilao_combate FOREIGN KEY (vilao) REFERENCES Viloes(vilao) ON DELETE CASCADE,
+    CONSTRAINT fk_local_combate FOREIGN KEY (localizacao) REFERENCES Localizacoes(regiao) ON DELETE CASCADE
 );
 
 CREATE TABLE Vitimas(
+    cpf VARCHAR(14),
     localizacao VARCHAR(80) NOT NULL,
     heroi VARCHAR(80) NOT NULL,
     vilao VARCHAR(20) NOT NULL,
     dia DATE NOT NULL,
-    cpf VARCHAR(14) PRIMARY KEY,
-    FOREIGN KEY (heroi, vilao, dia, localizacao),
-    REFERENCES Combates(heroi, vilao, dia, localizacao)
+    CONSTRAINT pk_vitimas PRIMARY KEY (cpf),
+    CONSTRAINT fk_combate_vitima FOREIGN KEY (heroi, vilao, dia, localizacao)
+    REFERENCES Combates(heroi, vilao, dia, localizacao) ON DELETE CASCADE
 );
 
+INSERT INTO Herois(heroi) VALUES('Nick Fury');
 INSERT INTO Herois(heroi) VALUES('Iron Man');
 INSERT INTO Herois(heroi) VALUES('Captain America');
 INSERT INTO Herois(heroi) VALUES('Hulk');
